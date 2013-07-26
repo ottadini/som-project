fff1fb7fc65ff0d7b301994ee7ff7bef2e355757
Ben Harrison <ben.harrison@liquidmesh.com>
2013-07-26 15:19:34 +1000

    First properly working version, with un-scaling working, no lists where should be vectors, mostly sorted out plots, got rid of some duplication. Next steps: more visualisations, and some measures of 'accuracy' of predictions.

--- som-prediction-gist.R
+++ som-prediction-gist.R
@@ -19,26 +19,23 @@ testing <- scale(somdata[-inTrain, ],
 # somdata[1] is the MEAS_TC variable
 somX <- training
 somY <- training[, 1]
-somdata.xyf <- xyf(data=training, Y=somY, 
-                   xweight=0.2, grid=somgrid(5, 5, "hexagonal"), contin=TRUE)
+somdata.xyf <- xyf(data=training, Y=somY, contin=TRUE, rlen=500, 
+                   xweight=0.2, grid=somgrid(5, 5, "hexagonal"))
 
-# Prediction with test set:
-somdata.xyf.prediction <- predict(somdata.xyf, newdata=testing)
 
 # Follow example from Wehrens & Buydens 2007 page 9:
 # Should the following be $unit.prediction, $unit.predictions, or $prediction ??
 tc.xyf <- predict(somdata.xyf, newdata=testing)$prediction  # Y codebook vectors
 tc.predict <- as.numeric(tc.xyf)
 plot(somdata.xyf, type="property", property=tc.predict, main="Prediction of TC")
-
+plot(somdata.xyf, type="changes")
 
 # Basic plot:
 par(mfrow=c(1,1))
-plot.new()
 x <- seq(nrow(testing))
 plot(x, testing[, "MEAS_TC"], type="l", col="black", ylim=c(-2, 2))
 par(new=TRUE)
-plot(x, somdata.xyf.prediction$prediction, type="l", col="red", ylim=c(-2, 2))
+plot(x, tc.predict, type="l", col="red", ylim=c(-2, 2))
 
 # Plot of codebook vectors
 par(mfrow=c(1, 2))
@@ -47,26 +44,26 @@ plot(somdata.xyf, type="codes")
 # Pre-scaled (original) meas_tc data:
 meas.tc.testing <- somdata[-inTrain, "MEAS_TC"]
 
-# Try to unscale the predicted tc:
+# Un-scale() the predicted tc:
 descale <- attr(testing, 'scaled:scale')[["MEAS_TC"]]
 decentre <- attr(testing, 'scaled:center')[["MEAS_TC"]]
-predicted.tc <- lapply(somdata.xyf.prediction$prediction, function(x) x * descale + decentre)
+tc.predict.descale <- sapply(tc.predict, function(x) x * descale + decentre)
 
 # Basic plot:
-plot.new()
+par(mfrow=c(1, 1))
 plot(x, meas.tc.testing, type="l", col="black", ylim=c(0, 3))
 par(new=TRUE)
-plot(x, predicted.tc, type="l", col="red", ylim=c(0, 3))
+plot(x, tc.predict.descale, type="l", col="red", ylim=c(0, 3))
 
 var(meas.tc.testing)
-var(as.numeric(predicted.tc))
-cor(meas.tc.testing, as.numeric(predicted.tc), method="kendall")
-cor(meas.tc.testing, as.numeric(predicted.tc), method="spearman")
-cov(meas.tc.testing, as.numeric(predicted.tc))
+var(tc.predict.descale)
+cor(meas.tc.testing, tc.predict.descale, method="kendall")
+cor(meas.tc.testing, tc.predict.descale, method="spearman")
+cov(meas.tc.testing, tc.predict.descale)
 
 RMS <- function(num) sqrt(sum(num^2)/length(num))
 
 RMS(meas.tc.testing)
-RMS(as.numeric(predicted.tc))
+RMS(tc.predict.descale)
 
 # ===== #
